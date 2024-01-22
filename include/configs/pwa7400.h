ee2e2c677f63b19ebd3596c759b8ed8832529439
Suguru Saito <sg.sgch07@gmail.com>
2024-01-22 21:30:23 +0900

    sharp: fix PW-A7400 support

---
+++ include/configs/pwa7400.h
@@ -0,0 +1,25 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * (C) Copyright 2024 Suguru Saito.
+ * Author: Suguru Saito <sg.sgch07@gmail.com>
+ *
+ * (C) Copyright 2011 Takumi Sueda.
+ * Author: Takumi Sueda <puhitaku@gmail.com>
+ *
+ * (C) Copyright 2011 Freescale Semiconductor, Inc.
+ * Author: Fabio Estevam <fabio.estevam@freescale.com>
+ *
+ * Based on m28evk.h:
+ * Copyright (C) 2011 Marek Vasut <marek.vasut@gmail.com>
+ * on behalf of DENX Software Engineering GmbH
+ */
+#ifndef __CONFIGS_PWA7400_H__
+#define __CONFIGS_PWA7400_H__
+
+/* Common configurations of Brain 2G, 3G, 4G */
+#include <configs/brain_mx28_common.h>
+
+/* The rest of the configuration is shared */
+#include <configs/mxs.h>
+
+#endif /* __CONFIGS_PWA7400_H__ */
